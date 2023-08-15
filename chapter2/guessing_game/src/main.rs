4eadd6156722f8b367925004e1222f7a4423e57c
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-15 15:34:46 -0600

    the loop keyword creates an infinite loop

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -5,25 +5,27 @@ use rand::Rng;
 fn main() {
     println!("Guess the number!");
 
-    println!("Please input your guess.");
+    loop {
+        println!("Please input your guess.");
 
-    let secret_number = rand::thread_rng().gen_range(1..101);
+        let secret_number = rand::thread_rng().gen_range(1..101);
 
-    println!("The secret number is: {}", secret_number);
+        println!("The secret number is: {}", secret_number);
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Fail to read line");
+        io::stdin().read_line(&mut guess)
+            .expect("Fail to read line");
 
-    let guess: u32 = guess.trim().parse()
-        .expect("Please type a number!");
+        let guess: u32 = guess.trim().parse()
+            .expect("Please type a number!");
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too Small!"),
-        Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("You Win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too Small!"),
+            Ordering::Greater => println!("Too Big!"),
+            Ordering::Equal => println!("You Win!"),
+        }
     }
 }
