eb798f2e68a35453c0eba06f2b8d2d020ecfbbd4
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-15 15:33:20 -0600

    convert the String the program reads as input into a real number type so we can compare it numerically to the secret number

--- chapter2/guessing_game/src/main.rs
+++ chapter2/guessing_game/src/main.rs
@@ -16,11 +16,14 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Fail to read line");
 
+    let guess: u32 = guess.trim().parse()
+        .expect("Please type a number!");
+
     println!("You guessed: {}", guess);
 
-    match guess.cmp($secret_number) {
+    match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too Small!"),
         Ordering::Greater => println!("Too Big!"),
-        Ordering::Equal => println!("Too win!"),
+        Ordering::Equal => println!("You Win!"),
     }
 }
