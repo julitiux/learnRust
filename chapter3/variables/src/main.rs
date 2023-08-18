6ef0e642c80a698aa73ca662ec8a146e1d39de52
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-17 19:51:24 -0600

    shadowing

--- chapter3/variables/src/main.rs
+++ chapter3/variables/src/main.rs
@@ -5,4 +5,11 @@ fn main() {
     x = 6;
     println!("The value of x is {}", x);
 
+    // Shadowing
+
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
 }
