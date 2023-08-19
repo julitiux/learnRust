fb1e30b72efcf49678ffe027959ea8c1e9b800c2
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-18 22:41:04 -0600

    destructuring

--- chapter3/variables/src/main.rs
+++ chapter3/variables/src/main.rs
@@ -86,11 +86,22 @@ fn main() {
 
 
     println!("\nTUPLA TYPE \n");
+
     let tup: (i32, f64, u8) = (500, 6.4, 1);
     let (x, y, z) = tup;
 
     println!("The value of x: {}", x);
     println!("The value of y: {}", y);
     println!("The value of z: {}", z);
+
+    let x :  (i32, f64, u8) = (500, 6.4, 1);
+
+    let five_hundred = x.0;
+    let six_point_four = x.1;
+    let one = x.2;
+
+    println!("\nfive hundred {}", five_hundred);
+    println!("six point four {}", six_point_four);
+    println!("one {}", one);
 
 }
