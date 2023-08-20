ac03155a22c70b35f3342b9aa9a9c303aa58696f
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-19 23:51:45 -0600

    functions with return values

--- chapter3/functions/src/main.rs
+++ chapter3/functions/src/main.rs
@@ -4,6 +4,8 @@ fn main() {
     another_function();
     another_function1(5, 6);
 
+    println!("function five {}", five());
+
 }
 
 fn another_function() {
@@ -13,4 +15,8 @@ fn another_function() {
 fn another_function1(x: i32, y: i32){
     println!("The value of x is: {}", x);
     println!("The value of y is: {}", y);
+}
+
+fn five() -> i32 {
+    5
 }
