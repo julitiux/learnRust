4e2abefe9deb4bf9968d4c2ed3f17c7d4fd56b0b
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-20 01:05:27 -0600

    expected 'bool' found integer

--- chapter3/branches/src/main.rs
+++ chapter3/branches/src/main.rs
@@ -2,10 +2,8 @@ fn main() {
 
     let number = 7;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    if number {
+        println!("number was three");
     }
 
 }
