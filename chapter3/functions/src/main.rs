2e7ce3f90624594b6822b248c194b840b3c0fbe9
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-19 23:32:13 -0600

    function with multiple parameters

--- chapter3/functions/src/main.rs
+++ chapter3/functions/src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
-    another_function1(5);
+    another_function1(5, 6);
 
 }
 
@@ -10,6 +10,7 @@ fn another_function() {
     println!("Another Function");
 }
 
-fn another_function1(x: i32){
+fn another_function1(x: i32, y: i32){
     println!("The value of x is: {}", x);
+    println!("The value of y is: {}", y);
 }
