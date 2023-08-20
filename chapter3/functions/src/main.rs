bc507063f1fd3c9c342b1a6c3e7ac5351c60cdaa
Julio Ramirez <rrodriguez.julio@gmail.com>
2023-08-19 23:29:04 -0600

    functiona parameters

--- chapter3/functions/src/main.rs
+++ chapter3/functions/src/main.rs
@@ -2,8 +2,14 @@ fn main() {
     println!("Hello, world!");
 
     another_function();
+    another_function1(5);
+
 }
 
 fn another_function() {
     println!("Another Function");
+}
+
+fn another_function1(x: i32){
+    println!("The value of x is: {}", x);
 }
